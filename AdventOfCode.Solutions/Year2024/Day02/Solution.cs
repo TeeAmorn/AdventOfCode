a687144a4ced3a3f206ad3c8ca4335592d2b93c8
Tee Amornkasemwong <theerut.amorn@gmail.com>
2025-12-03 11:23:19 -0800

    Solve puzzle from year 2024 day 2

--- AdventOfCode.Solutions/Year2024/Day02/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day02/Solution.cs
@@ -1,14 +1,42 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace AdventOfCode.Solutions.Year2024.Day02;
 
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
 public class Solution : ISolution
 {
-    public string SolvePartOne(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public string SolvePartOne(string input) =>
+        ParseReports(input)
+            .Count(IsValid)
+            .ToString();
 
-    public string SolvePartTwo(string input)
+    public string SolvePartTwo(string input) =>
+        ParseReports(input)
+            .Count(report => GenerateReportVariations(report).Any(IsValid))
+            .ToString();
+
+    private static IEnumerable<int[]> ParseReports(string input) =>
+        input
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(line =>
+                line
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray());
+
+    private static IEnumerable<int[]> GenerateReportVariations(int[] report) =>
+        Enumerable
+            .Range(0, report.Length)
+            .Select(i =>
+                report
+                    .Take(i)
+                    .Concat(report.Skip(i + 1))
+                    .ToArray());
+
+    private static bool IsValid(int[] report)
     {
-        throw new NotImplementedException();
+        var pairs = report.Zip(report.Skip(1)).ToList();
+        return pairs.All(p => p.First - p.Second >= 1 && p.First - p.Second <= 3) ||
+               pairs.All(p => p.Second - p.First >= 1 && p.Second - p.First <= 3);
     }
 }
