a792a848aeafc3d2f51420e09cb1ddc1e8d57aae
Tee Amornkasemwong <theerut.amorn@gmail.com>
2025-12-03 10:11:02 -0800

    Solve puzzle from year 2024 day 1

--- AdventOfCode.Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day01/Solution.cs
@@ -1,14 +1,50 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace AdventOfCode.Solutions.Year2024.Day01;
 
+[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)]
 public class Solution : ISolution
 {
     public string SolvePartOne(string input)
     {
-        throw new NotImplementedException();
+        (List<int> left, List<int> right) = ([], []);
+
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(values[0]));
+            right.Add(int.Parse(values[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        return left
+            .Zip(right, (l, r) => Math.Abs(l - r))
+            .Sum()
+            .ToString();
     }
 
     public string SolvePartTwo(string input)
     {
-        throw new NotImplementedException();
+        List<int> left = [];
+        Dictionary<int, uint> right = new();
+
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var line in lines)
+        {
+            var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(values[0]));
+
+            var rightValue = int.Parse(values[1]);
+            if (!right.TryAdd(rightValue, 1))
+                right[rightValue]++;
+        }
+
+        return left
+            .Select(l => l * (right.TryGetValue(l, out var value) ? value : 0))
+            .Sum()
+            .ToString();
     }
 }
